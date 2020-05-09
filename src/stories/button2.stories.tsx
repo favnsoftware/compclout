3427e731b947ec1de707902af6b901f107321ae2
Sveinung Øverland <39273837+SveinungOverland@users.noreply.github.com>
2020-05-09 20:45:00 +0200

    Create button2.stories.tsx

---
+++ src/stories/button2.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Button } from '../components/button'
+import { action } from '@storybook/addon-actions'
+import { withKnobs, text, boolean } from '@storybook/addon-knobs'
+
+export default {
+    title: "Button2",
+    decorators: [withKnobs]
+}
+
+export const primary = () => {
+    const label = text("Label", "|See now!")
+    const outlined = boolean("Outlined", false)
+    return (
+        <Button 
+            onClick={action('clicked')} 
+            outlined={outlined}
+            label={label}
+        />
+    )
+}
