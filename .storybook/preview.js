73279998ba726f609460e7cc4a5fad15013c04bd
Sveinung Øverland <39273837+SveinungOverland@users.noreply.github.com>
2020-05-09 13:11:38 +0200

    Porject init

---
+++ .storybook/preview.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { addDecorator } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import Layout from './Layout'
+
+addDecorator(storyFn => <Layout>{storyFn()}</Layout>)
+addDecorator(withInfo({
+    inline: true,
+    styles: (base) => ({
+        ...base,
+        infoBody: {
+            ...base.infoBody,
+        },
+        source: {
+            h1: {
+                margin: '20px 0',
+                padding: '0 0 5px 0',
+                fontSize: '25px',
+                borderBottom: '1px solid #EEE',
+            },
+        },
+    }),
+    propTablesExclude: [Layout]
+}))
