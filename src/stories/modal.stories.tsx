0c596b557f7fab64f3de37c88edea9fe1b17c2e3
William Peter Åredal <williamaredal@hotmail.com>
2020-06-25 14:57:20 +0200

    Update modal.stories.tsx

--- src/stories/modal.stories.tsx
+++ src/stories/modal.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Modal } from '../components'
-import { withKnobs, boolean, action } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
+import { withKnobs, boolean } from '@storybook/addon-knobs'
 
 export default {
     title: "Modal",
@@ -9,11 +10,16 @@ export default {
 
 
 export const primary = () => {
+    const childExample = <div className='bg-white paper rounded text-center'>
+            <h1>This is a Modal Example</h1>
+            <p>Modal can display a variety of child component types. When clicking outside this Modal child component onBackdropClick will trigger.</p>
+            <p>The Modal can also be centered, showed or hidden.</p>
+        </div>
     return (
         <div>
             <Modal
-            show
-            children
+            show={boolean('Show Modal', false)}
+            children={childExample}
             center={boolean('Center Modal', true)}
             onBackdropClick={action('Backdrop Clicked')}
             />
