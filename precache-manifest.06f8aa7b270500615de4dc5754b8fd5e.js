5ce76803a7a91fb80d754b6421e9c82a8ba42b0b
Github Action <action@github.com>
2020-06-26 13:16:36 +0000

    gh-pages

---
+++ precache-manifest.06f8aa7b270500615de4dc5754b8fd5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2851d375f21793546fc310f364bd1979",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a5da6beb54bc5ac00816.bundle.js"
+  },
+  {
+    "url": "runtime~main.a5da6beb54bc5ac00816.bundle.js"
+  },
+  {
+    "revision": "28ee5750757621d9ded4",
+    "url": "static/css/main.a12ca7b2.chunk.css"
+  },
+  {
+    "revision": "8c1c8232b122b797068e",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.a5da6beb54bc5ac00816.bundle.js"
+  },
+  {
+    "url": "vendors~main.a5da6beb54bc5ac00816.bundle.js.LICENSE.txt"
+  }
+]);
